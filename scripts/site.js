e5bc8178b02ea45c52209960c61abc0fa86030c5
james <wepudaljamesbernard05@gmail.com>
2025-01-20 20:36:18 +0800

    added resume reference

--- scripts/site.js
+++ scripts/site.js
@@ -51,7 +51,7 @@ class App {
     _initHeroButtonEvents() {
         heroContainer.addEventListener('click', function (e) {
             const button = e.target.closest("a");
-            if (!button.classList.contains("social-link")) {
+            if (!button.classList.contains("social-link") && !button.dataset.btnHero === "resume") {
                 e.preventDefault();
             }
             if (button.dataset.btnHero === 'contact') {
